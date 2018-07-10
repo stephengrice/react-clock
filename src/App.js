76f6fa8c2efd6f97e390438e0184beab321c5fbb
Stephen Grice <stephengrice@live.com>
2018-07-09 20:03:08 -0400

    Import clock from CodePen experiment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Cube extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rot: 0,
+      value: 'default',
+      faceValue: [
+        this.props.value, '', '', '',
+      ],
+    }
+  }
+  change(newValue) {
+    if (newValue == this.state.value)
+       return;
+    var newFaces = this.state.faceValue;
+    newFaces[(this.getFace() + 1) % 4] = newValue;
+    this.setState({value: newValue, rot: (this.state.rot - 90)});
+  }
+  getFace() {
+    return Math.abs(this.state.rot / 90) % 4;
+  }
+  render() {
+    var myStyle = {
+      transform: 'rotateX('+this.state.rot+'deg)',
+    };
+    return (
+        <div className="cube" id="cube" style={myStyle}>
+          <div className="face face-front" id="front">{this.state.faceValue[0]}</div>
+          <div className="face face-top" id='top'>{this.state.faceValue[1]}</div>
+          <div className="face face-back" id='back'>{this.state.faceValue[2]}</div>
+          <div className="face face-bottom" id='bottom'>{this.state.faceValue[3]}</div>
+        </div>
+      );
+  }
+}
+class CubeClock extends Component {
+  constructor(props) {
+    super(props);
+    this.cubeHour = React.createRef();
+    this.cubeMin = React.createRef();
+    this.cubeSec = React.createRef();
+    this.cubeTod = React.createRef();
+
+    this.state = {
+      date: new Date()
+    }
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => {this.tick();}, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({date: new Date()});
+    this.cubeHour.current.change(pad(this.state.date.getHours() - 12));
+    this.cubeMin.current.change(pad(this.state.date.getMinutes()));
+    this.cubeSec.current.change(pad(this.state.date.getSeconds()));
+    this.cubeTod.current.change(tod(this.state.date));
+  }
+  render() {
+    return (
+      <div className="clock-wrap">
+      <div className="clock">
+        <Cube ref={this.cubeHour} value={pad(this.state.date.getHours() - 12)} />
+        <Cube ref={this.cubeMin} value={pad(this.state.date.getMinutes())} />
+        <Cube ref={this.cubeSec} value={pad(this.state.date.getSeconds())} />
+        <Cube ref={this.cubeTod} value={tod(this.state.date)} />
+      </div>
+      </div>
+     );
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <CubeClock />;
   }
+}
+
+function pad(num) {
+  return num < 10 ? '0' + num : num;
+  }
+function tod(date) {
+  return date.getHours() < 12 ? 'AM' : 'PM';
 }
 
 export default App;
